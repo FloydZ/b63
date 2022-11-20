ae54217674fd2c41677c1a0e348237faee300f9b
github-action-benchmark <github@users.noreply.github.com>
2022-11-20 17:44:18 +0000

    add b63 (customSmallerIsBetter) benchmark result for bbbee4b8924b391be0a45b2f2cc938e7ec952af7

---
+++ dev/bench/data.js
@@ -0,0 +1,42 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1668966258475,
+  "repoUrl": "https://github.com/FloydZ/b63",
+  "entries": {
+    "b63": [
+      {
+        "commit": {
+          "author": {
+            "email": "floyd.zweydinger@rub.de",
+            "name": "Floyd Zweydinger"
+          },
+          "committer": {
+            "email": "floyd.zweydinger@rub.de",
+            "name": "Floyd Zweydinger"
+          },
+          "distinct": true,
+          "id": "bbbee4b8924b391be0a45b2f2cc938e7ec952af7",
+          "message": "changed tool",
+          "timestamp": "2022-11-20T18:43:47+01:00",
+          "tree_id": "b042b7eff604df5414d69fabd67a2b8bdb1b6808",
+          "url": "https://github.com/FloydZ/b63/commit/bbbee4b8924b391be0a45b2f2cc938e7ec952af7"
+        },
+        "date": 1668966257987,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "time",
+            "value": 21.099761,
+            "unit": "events/s",
+            "extra": "basic"
+          },
+          {
+            "name": "time",
+            "value": 10.446008,
+            "unit": "events/s",
+            "extra": "basic_half (-50.492294)42"
+          }
+        ]
+      }
+    ]
+  }
+}
